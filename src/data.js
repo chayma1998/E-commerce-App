516acbdf14dd3abc48ffe465f69200b6c0a7bc51
chayma1998 <57661841+chayma1998@users.noreply.github.com>
2022-05-03 12:48:11 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,108 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:"https://www.nicepng.com/png/full/8-88585_women-fashion-png-example-of-magazine-cover.png",
+        title: "SUMMER SALE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    
+    },
+
+    {
+        id:2,
+        img:"https://i.ibb.co/DG69bQ4/2.png",
+        title: "AUTUMN COLLECTION",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",   
+    },
+
+    { 
+        id:3,
+        img:"https://i.ibb.co/cXFnLLV/3.png",
+        title: "LOUNGEWEAR LOVE",
+        desc:"DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+
+
+    },
+];
+
+export const categories =[
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/5886038/pexels-photo-5886038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "SHIRT STYLE!",
+        cat:"summer"
+        
+    
+    },
+
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/7162024/pexels-photo-7162024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "LOUNGEWEAR LOVE",
+        cat:"woman"
+           
+    },
+
+    { 
+        id:3,
+        img:"https://images.pexels.com/photos/11569078/pexels-photo-11569078.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title: "LIGHT JACKETS",
+        cat:"winter"
+        
+
+
+    },
+];
+ 
+export const popularProducts = [
+
+    {
+        id:1,
+        img:"https://images.pexels.com/photos/4450350/pexels-photo-4450350.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        
+    
+    },
+
+    {
+        id:2,
+        img:"https://images.pexels.com/photos/6311647/pexels-photo-6311647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        
+           
+    },
+
+    { 
+        id:3,
+        img:"https://images.pexels.com/photos/6311637/pexels-photo-6311637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        
+         
+    },
+
+    {
+        id:4,
+        img: "https://images.pexels.com/photos/6923343/pexels-photo-6923343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        id:5,
+        img: "https://images.pexels.com/photos/6995895/pexels-photo-6995895.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        id:6,
+        img: "https://images.pexels.com/photos/11075319/pexels-photo-11075319.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        id:7,
+        img: "https://images.pexels.com/photos/7615247/pexels-photo-7615247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        id:8,
+        img: "https://images.pexels.com/photos/5707691/pexels-photo-5707691.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+];
+
+
